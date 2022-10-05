0e43a6b9e203cca4c4d7854fced1717387e69234
shiva <kumarshiva0707@gmail.com>
2022-10-05 14:42:59 +0530

    first react project

---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <div>
+                <h1>This is  First React Project</h1>
+                <p>
+                    <img src="https://picsum.photos/300/150?programming" />
+                </p>
+                <h1>How is this 😊💚</h1>
+            </div>
+            
+        </div>
+    );
+}
+export default Note;
